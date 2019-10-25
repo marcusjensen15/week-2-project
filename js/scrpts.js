4bf58229f5d8f108aedc8ddfc2843d78511d6ccc
Marcus Jensen <marcusjensen1515@gmail.com>
2019-10-25 08:19:30 -0700

    outline basic requirements of application within js file.

---
+++ js/scrpts.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Project Requirements:
+//
+// -Collect input from user via radial buttons on form submit. Assign a value to each user input;
+// -Create variables for each user input;
+// -Create a variable that serves as a 'counter'. To contain the user's final score.
+// -Create a function to calculate the user's final score.
+// -Use branching to determine which language is best for that user based on their score.
+// -Show result of score based on user input.
