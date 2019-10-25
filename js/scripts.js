db02cae5d2ef53b6600f9b5dd3de45d628957058
Marcus Jensen <marcusjensen1515@gmail.com>
2019-10-25 09:21:24 -0700

    Define strategy for calculating values of radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
 
-    var questionOneTotal = parseInt($("input#question1").val()) + parseInt($("input#question2").val()) + parseInt($("input#question3").val());
-    console.log(questionOneTotal);
+    // var questionOneTotal = parseInt($("input#question1").val()) + parseInt($("input#question2").val()) + parseInt($("input#question3").val());
+    // console.log(questionOneTotal);
+
+    // var questionOneTotal = parseInt($("input:radio[id=question1]:checked").val()) + parseInt($("input:radio[id=question2]:checked").val());
+    // console.log(questionOneTotal);
+
+    if ("input:radio[id=question1]:checked"){
+      var value1 = 5;
+      }
+      else {
+        value1 = 0;
+      }
+
+      console.log(value1);
 
 
-
-
+// input:radio[name=operator]:checked"
 
   event.preventDefault();
   });
@@ -24,3 +35,5 @@ $(document).ready(function() {
 // -Create a function to calculate the user's final score.
 // -Use branching to determine which language is best for that user based on their score.
 // -Show result of score based on user input.
+
+//Create chain of if-else statements seeing if the box is checked for each option. If an option is selected, assign value, if else assign zero
