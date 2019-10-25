f56df05b9cfc86f43bceab7aa0da808caa847844
Marcus Jensen <marcusjensen1515@gmail.com>
2019-10-25 10:09:00 -0700

    add additional questions

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,8 @@ $(document).ready(function() {
     var value2 = parseInt($("#question2").val());
 
     console.log(value1);
-
     console.log(value2);
+    console.log(value1 +value2);
 
 
 // input:radio[name=operator]:checked"
